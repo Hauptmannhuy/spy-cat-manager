41c52e9f22578b8d651ce1785b0e6eaebd6b5509
hauptmannhuy <dindago2324@gmail.com>
2025-02-26 14:46:13 +0000

    add response logger

--- middleware.go
+++ middleware.go
@@ -16,6 +16,11 @@ type routeMiddleware struct {
 	router http.Handler
 }
 
+type responseWriter struct {
+	writer http.ResponseWriter
+	status int
+}
+
 func initMiddleware(app *application) *middleware {
 
 	return &middleware{
@@ -30,19 +35,40 @@ func initRoutes(app *application) http.Handler {
 	router := http.NewServeMux()
 	router.HandleFunc("GET /spy/{id}/", app.getCat)
 	router.HandleFunc("POST /spy", app.createCat)
+
 	return router
 }
 
 func (middleware *middleware) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	middleware.logMiddleware.log(r)
-	middleware.routeMiddleware.router.ServeHTTP(w, r)
+	middleware.logMiddleware.logRequest(r)
+	rw := &responseWriter{
+		writer: w,
+	}
+	middleware.routeMiddleware.router.ServeHTTP(rw, r)
+	middleware.logMiddleware.logResponse(rw, r)
+
 }
 
-func (logMid *loggingMiddleware) log(r *http.Request) {
-	if r.Response == nil {
-		fmt.Printf("New request, method: %s, request URI: %s\n", r.Method, r.URL)
-	} else {
-		fmt.Printf("Response: status - %s", r.Response.Status)
-	}
+func (logMid *loggingMiddleware) logRequest(r *http.Request) {
+	fmt.Printf("New request, method: %s, request URI: %s\n", r.Method, r.URL)
+}
 
+func (logMid *loggingMiddleware) logResponse(rw *responseWriter, r *http.Request) {
+	fmt.Printf("Response - method: %s, request URI: %s, status - %d\n", r.Method, r.URL, rw.status)
+}
+
+func (rw responseWriter) Header() http.Header {
+	return rw.writer.Header()
+}
+func (rw *responseWriter) Write(p []byte) (int, error) {
+	n, err := rw.writer.Write(p)
+	if err != nil {
+		fmt.Println(err)
+	}
+	return n, nil
+}
+
+func (rw *responseWriter) WriteHeader(code int) {
+	rw.writer.WriteHeader(code)
+	rw.status = code
 }
